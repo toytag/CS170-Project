e3ba4de1831cabb92ee0844a75dfa8c5a0469e8f
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-05-03 04:16:04 -0700

    multiprocessing

--- max_st.py
+++ max_st.py
@@ -2,16 +2,27 @@ from parse import *
 import networkx as nx
 import os
 from solver import solve
-import multiprocessing
+from multiprocessing import Pool
+
+def compute(input_path):
+    graph_name = input_path.split(".")[0]
+    # print(cnt, graph_name, f"\t{cnt/1006*100 : .2f}%", end="\r")
+    # cnt += 1
+    print(graph_name)
+    G = read_input_file(f"{input_dir}/{input_path}")
+    T = solve(G)
+    write_output_file(T, f"{output_dir}/{graph_name}.out")
 
 if __name__ == "__main__":
     output_dir = "outputs"
     input_dir = "inputs"
-    cnt = 1
-    for input_path in os.listdir(input_dir):
-        graph_name = input_path.split(".")[0]
-        print(cnt, graph_name, f"\t{cnt/1006*100 : .2f}%", end="\r")
-        cnt += 1
-        G = read_input_file(f"{input_dir}/{input_path}")
-        T = solve(G)
-        write_output_file(T, f"{output_dir}/{graph_name}.out")
+    # cnt = 1
+    pool = Pool()
+    pool.map(compute, os.listdir(input_dir))
+    # for input_path in os.listdir(input_dir):
+    #     graph_name = input_path.split(".")[0]
+    #     # print(cnt, graph_name, f"\t{cnt/1006*100 : .2f}%", end="\r")
+    #     # cnt += 1
+    #     G = read_input_file(f"{input_dir}/{input_path}")
+    #     T = solve(G)
+    #     write_output_file(T, f"{output_dir}/{graph_name}.out")
