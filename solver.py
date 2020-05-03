ea9e91eceee8b066fa692208a47cc05f53bb70ee
Zhenzhong Tang <ztang@berkeley.edu>
2020-05-03 07:06:07 +0000

    update

--- solver.py
+++ solver.py
@@ -118,12 +118,6 @@ def solve(G):
     # return N
 
 
-
-
-
-
-
-
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
@@ -135,4 +129,4 @@ if __name__ == '__main__':
     T = solve(G)
     assert is_valid_network(G, T)
     print("Average pairwise distance: {}".format(average_pairwise_distance(T)))
-    write_output_file(T, 'outputs/test.out')
+    write_output_file(T, 'test.out')
